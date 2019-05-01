43aeafb9440e18b7ee495c3f07136bdcf083bcb7
nartzaa <39677740+nartzaa@users.noreply.github.com>
2019-05-02 01:32:26 +0700

    Add files via upload

---
+++ 128X64_OLED_LCD_LED_Display_Module.ino
@@ -0,0 +1,42 @@
+/*
+
+การต่อใช้งาน
+
+Vcc - > 3.3 / 5 V
+Gnd -> Gnd
+SCL -> A5
+SDA -> A4
+
+*/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+void setup() {
+display.begin(SSD1306_SWITCHCAPVCC, 0x3c); //initialize I2C addr 0x3c
+display.clearDisplay(); // clears the screen and buffer
+display.drawPixel(127, 63, WHITE);
+
+
+display.drawLine(0, 63, 127, 21, WHITE);
+display.drawCircle(110, 50, 12, WHITE);
+display.fillCircle(45, 50, 8, WHITE);
+display.drawTriangle(70, 60, 90, 60, 80, 46, WHITE);
+display.setTextSize(1);
+display.setTextColor(WHITE);
+display.setCursor(0,0);
+display.println("Kampanart.Ch");
+display.setTextSize(2);
+display.println("ArduinoAll");
+display.setTextColor(BLACK, WHITE);
+display.setTextSize(1);
+display.println("www.ArduinoAll.com");
+display.setTextColor(WHITE, BLACK);
+display.display();
+}
+
+void loop() {
+}
