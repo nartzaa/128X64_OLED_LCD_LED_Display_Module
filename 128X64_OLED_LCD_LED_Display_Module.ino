a825fc1fcda02ae51c199c678e8d494b8d200a4f
nartzaa <nartzaa@users.noreply.github.com>
2020-04-18 12:05:15 +0700

    Update 128X64_OLED_LCD_LED_Display_Module.ino
    Edit code

--- 128X64_OLED_LCD_LED_Display_Module.ino
+++ 128X64_OLED_LCD_LED_Display_Module.ino
@@ -8,34 +8,40 @@ SCL -> A5
 SDA -> A4
 
 */
-
+#include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Adafruit_SSD1306.h>   // Setting screen size in library
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
+
 void setup() {
-display.begin(SSD1306_SWITCHCAPVCC, 0x3c); //initialize I2C addr 0x3c
-display.clearDisplay(); // clears the screen and buffer
-display.drawPixel(127, 63, WHITE);
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3c); //initialize I2C addr 0x3c
+    display.clearDisplay(); // clears the screen and buffer
+    /*display.drawPixel(0, 0, WHITE);
+    display.drawPixel(127, 63, WHITE);*/
 
+    //display.drawCircle(64, 15, 8, WHITE);
+    //display.fillCircle(64, 32, 30, WHITE);
+    //display.drawTriangle(70, 60, 90, 60, 80, 46, WHITE);
 
-display.drawLine(0, 63, 127, 21, WHITE);
-display.drawCircle(110, 50, 12, WHITE);
-display.fillCircle(45, 50, 8, WHITE);
-display.drawTriangle(70, 60, 90, 60, 80, 46, WHITE);
-display.setTextSize(1);
-display.setTextColor(WHITE);
-display.setCursor(0,0);
-display.println("Kampanart.Ch");
-display.setTextSize(2);
-display.println("ArduinoAll");
-display.setTextColor(BLACK, WHITE);
-display.setTextSize(1);
-display.println("www.ArduinoAll.com");
-display.setTextColor(WHITE, BLACK);
-display.display();
+    display.setCursor(0,0);
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.println("Kampanart.Ch");
+
+    display.setTextSize(2);
+    display.setTextColor(BLACK, WHITE);
+    display.println("ArduinoAll");    
+    display.setTextSize(1);
+    display.println("www.ArduinoAll.com");
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE, BLACK);
+    display.println("OLED 128x64");
+    
+    display.display();
 }
 
 void loop() {
